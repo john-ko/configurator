7fbe52d55455d64825c2e287bd457ec8bb40bbe3
John Ko <johnko426@gmail.com>
2023-10-05 21:08:20 -0700

    chore: remove unused comments

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,6 @@ const config = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
     '^.+\\.tsx?$': [
       'ts-jest',
       {
